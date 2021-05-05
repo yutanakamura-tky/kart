aa6960feb28d5ecde787e4fe3f5317cdec8907dd
yutanakamura-tky <yutanakamura.tky@gmail.com>
2021-05-05 14:54:33 +0900

    KART-07: extraction of disease names from full name mentions

--- src/modules/privacy/extract_diseases_from_mimic_iii_dummy_phi.py
+++ src/modules/privacy/extract_diseases_from_mimic_iii_dummy_phi.py
@@ -1,70 +1,101 @@
-import argparse
+import itertools
+import logging
+import pathlib
 import re
 from collections import OrderedDict
-from typing import Dict, List, Tuple
+from typing import Dict, List, Tuple, Union
 
 import nltk
 import pandas as pd
 from tqdm import tqdm
 
+from kart.src.modules.logging.logger import get_stream_handler
 from kart.src.modules.privacy.utils.path import get_repo_dir
 from kart.src.modules.pymetamap import pymetamap
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+stream_handler = get_stream_handler()
+file_handler = logging.FileHandler(f"{pathlib.Path(__file__).stem}.log")
+logger.addHandler(stream_handler)
+logger.addHandler(file_handler)
+
 
 def main():
-    args = get_args()
-    input_path = (
-        get_repo_dir()
-        / f"corpus/MIMIC_III_DUMMY_PHI_{args.corpus.upper()}_{args.code.upper()}.csv"
-    )
-    output_path = (
-        get_repo_dir() / f"corpus/gold_disease_names_{args.corpus}_{args.code}.tsv"
-    )
+    mm = get_metamap_instance()
+    for mode in ("hospital", "shadow"):
+        for code in ("c1p2", "c1p1", "c1p0", "c0p2", "c0p1", "c0p0"):
+            input_path = (
+                get_repo_dir()
+                / f"corpus/full_name_mentions_{mode.lower()}_{code.lower()}.tsv"
+            )
+            output_path = (
+                get_repo_dir()
+                / f"corpus/gold_disease_names_{mode.lower()}_{code.lower()}.tsv"
+            )
 
-    print(f"Loading {input_path} ...")
-    df = pd.read_csv(input_path, quoting=0)
+            logger.info(f"Loading {input_path} ...")
 
-    df_target = df.query("patient_full_name_tfreq>0")
+            try:
+                df = pd.read_csv(input_path, sep="\t")
+            except FileNotFoundError:
+                logger.error(
+                    f"File does not exist ({input_path}): "
+                    + "Please run extract_full_name_mentions_from_mimic_iii_dummy_phi.py first"
+                )
+                return None
 
-    # subject_ids = df_target["SUBJECT_ID"].values
-    full_name_mentions = df_target["full_name_mention"].values
-    patient_ages = df_target["patient_age"].values
-    patient_full_names = df_target["patient_full_name"].values
+            full_name_mentions = df["full_name_mention"].values
+            texts = [text.replace("\n", "") for text in full_name_mentions]
+            target_semantic_types = ["dsyn", "mobd", "neop"]
+            option = {"restrict_to_sts": target_semantic_types}
 
+            logger.info(
+                "Extracting UMLS concepts using MetaMap "
+                + f"(target semantic types = {', '.join(target_semantic_types)}) ..."
+            )
+            concepts: List[List[Dict]] = extract_umls_concepts(texts, mm, option)
+            logger.info("Done!")
+
+            chained_concepts: List[Dict] = list(itertools.chain(*concepts))
+            if chained_concepts:
+                logger.info(f"Sample: {chained_concepts[0]}")
+
+            logger.info("Retrieving superficial forms from full name mentions ...")
+            subject_ids = df["SUBJECT_ID"].values
+            patient_ages = df["patient_age"].values
+            patient_full_names = df["patient_full_name"].values
+            diseases = []
+
+            for i in tqdm(range(len(full_name_mentions))):
+                for entity in concepts[i]:
+                    superficials = entity["superficials"]
+                    concept: pymetamap.Concept.ConceptMMI = entity["concept"]
+
+                    dict_values = [
+                        ("document_id", i),
+                        ("subject_id", subject_ids[i]),
+                        ("patient_full_name", " ".join(eval(patient_full_names[i]))),
+                        ("patiant_age", patient_ages[i]),
+                        ("superficials", ", ".join(superficials)),
+                        ("semtypes", concept.semtypes),
+                        ("score", concept.score),
+                        ("cui", concept.cui),
+                        ("perferred_name", concept.preferred_name),
+                    ]
+                    disease = OrderedDict(dict_values)
+                    diseases.append(disease)
+
+            df_diseases = pd.DataFrame(diseases)
+            df_diseases.to_csv(output_path, index=False, sep="\t")
+            logger.info(f"Disease name saved to {output_path}")
+
+
+def get_metamap_instance() -> pymetamap.SubprocessBackend:
     mm = pymetamap.MetaMap.get_instance(
         "/home/nakamura/metamap/public_mm/bin/metamap20",
     )
-
-    texts = [text.replace("\n", "") for text in full_name_mentions]
-
-    option = {"restrict_to_sts": ["dsyn", "mobd", "neop"]}
-
-    results_with_option = extract_umls_concepts(texts, mm, option)
-
-    print("Extracting disease names ...")
-    diseases = []
-
-    for i in tqdm(range(len(full_name_mentions))):
-        for entity in results_with_option[i]:
-            superficials = entity["superficials"]
-            concept = entity["concept"]
-            disease = OrderedDict(
-                [
-                    ("document_id", i),
-                    ("patient_full_name", " ".join(eval(patient_full_names[i]))),
-                    ("patiant_age", patient_ages[i]),
-                    ("superficials", ", ".join(superficials)),
-                    ("score", concept.score),
-                    ("semtypes", concept.semtypes),
-                    ("cui", concept.cui),
-                    ("perferred_name", concept.preferred_name),
-                ]
-            )
-            diseases.append(disease)
-
-    df_diseases = pd.DataFrame(diseases)
-    df_diseases.to_csv(output_path, index=False, sep="\t")
-    print(f"Disease name saved to {output_path}")
+    return mm
 
 
 def extract_umls_concepts(
@@ -81,7 +112,8 @@ def extract_umls_concepts_from_one_text(
 ) -> List[Dict]:
     sentences = text_to_sentences(text)
     concepts, error = mm.extract_concepts(sentences, range(len(sentences)), **option)
-    entities = convert_concepts_to_superficial_concept_pair(concepts, sentences)
+    entities = convert_concepts_to_superficial_concept_pairs(concepts, sentences)
+    entities = drop_invalid_concepts(entities)
     return entities
 
 
@@ -91,37 +123,63 @@ def text_to_sentences(text: str) -> List[str]:
     return sentences
 
 
-def convert_concepts_to_superficial_concept_pair(
-    concepts: List[pymetamap.Concept.ConceptMMI], sentences: List[str]
-) -> List[Dict]:
-    entities = []
-    for concept in concepts:
-        sentence_id = int(concept.index)
-        raw_pos_info: List[Tuple[str, str]] = re.findall(
-            r"(\d+)/(\d+)", concept.pos_info
+def drop_invalid_concepts(superficial_concept_pairs: List[Dict]) -> List[Dict]:
+    """
+    MetaMap is sometimes too sensitive and recognizes non-disease spans as diseases.
+    This function gets rid of such excessive entries with rule based approach.
+    """
+    result = []
+    for pair in superficial_concept_pairs:
+        superficials = pair["superficials"]
+        non_disease_superficials = (
+            "was",
+            "IV",
+            "arms",
+            "call",
+            "NS",
+            "NC",
+            "a, o",
+            "this",
         )
-        positions = [
-            {
-                "start": int(pos_info[0]) - 1,
-                "end": int(pos_info[0]) + int(pos_info[1]) - 1,
-            }
-            for pos_info in raw_pos_info
-        ]
-        superficials = [
-            sentences[sentence_id][pos["start"] : pos["end"]] for pos in positions
-        ]
-        entities.append({"superficials": superficials, "concept": concept})
+        if set(superficials) & set(non_disease_superficials):
+            continue
+        else:
+            result.append(pair)
+    return result
+
+
+def convert_concepts_to_superficial_concept_pairs(
+    concepts: List[Union[pymetamap.Concept.ConceptMMI, pymetamap.Concept.ConceptAA]],
+    sentences: List[str],
+) -> List[Dict]:
+    """
+    Pymetamap does not return directly spans corresponding to each UMLS concept.
+    This function thus extracts such spans and returns pairs of UMLS concepts and spans.
+    """
+    entities = []
+
+    for concept in concepts:
+        if type(concept) is pymetamap.Concept.ConceptMMI:
+            sentence_id = int(concept.index)
+            raw_pos_info: List[Tuple[str, str]] = re.findall(
+                r"(\d+)/(\d+)", concept.pos_info
+            )
+            positions = [
+                {
+                    "start": int(pos_info[0]) - 1,
+                    "end": int(pos_info[0]) + int(pos_info[1]) - 1,
+                }
+                for pos_info in raw_pos_info
+            ]
+            superficials = [
+                sentences[sentence_id][pos["start"] : pos["end"]] for pos in positions
+            ]
+            entities.append({"superficials": superficials, "concept": concept})
+        elif type(concept) is pymetamap.Concept.ConceptAA:
+            continue
+        else:
+            continue
     return entities
-
-
-def get_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("corpus", choices=["hospital", "shadow"])
-    parser.add_argument(
-        "code", choices=["c1p2", "c1p1", "c1p0", "c0p2", "c0p1", "c0p0"]
-    )
-    args = parser.parse_args()
-    return args
 
 
 if __name__ == "__main__":
